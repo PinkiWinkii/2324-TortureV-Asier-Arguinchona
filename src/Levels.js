a3f65592b27e7bf48ae1aca9e10ad82a87fc42ab
PinkiWinki <asier.arguinchona@ikasle.aeg.eus>
2024-01-26 09:00:46 +0100

    Initial commit

---
+++ src/Levels.js
@@ -0,0 +1,13 @@
+export class Level
+{
+    constructor(data)
+    {
+        this.data       = data;     //Array bidimensional de datos del mapa
+    }
+
+}
+
+export const Level1 = 
+[
+    []
+]
---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const STATE = {
+    OVER: -1,
+    PLAYING: 0,
+}
+
+export const TILE = {
+    TILE_16: 0,
+}
+
+export const DATA = {
+    NUM_FIL: 12,
+    NUM_COL: 17,
+}
+
+export const BLOCK = {
+    PASILLO: 0,
+    MURO: 1,
+}
